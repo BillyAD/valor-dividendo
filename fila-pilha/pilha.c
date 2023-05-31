10e7e120c7ab5d2d75212583be3151a9fa009afb
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-05-31 18:06:38 -0300

    .

--- fila-pilha/pilha.c
+++ fila-pilha/pilha.c
@@ -6,7 +6,10 @@ int main(){
 
 	setlocale(LC_ALL,"Porguese");
 	
-	#define tamanho 10 
+	#define tamanho 10
+
+	int menu = 10;
+	int sair;
 
 	struct tpilha {
 		int dados[tamanho];
@@ -18,34 +21,75 @@ int main(){
 
 	/* Listar valores empilhados */
 	void pilha_conteudo() {
-		int i;
 		printf("[");
 		for (int i = 0; i < tamanho; i++) {
-			printf("%d, ", pilha.dados[i]);
+			printf("%d ", pilha.dados[i]);
 		}
 		printf("]");
 	}
 
 	/* Entrada de valores a serem empilhados */
 	void pilha_entrada() {
-		if (pilha.fim == tamanho) {
-			printf("A pilha está cheia\n");
-			return 0;
+		int entrada;
+		printf("Digite quantos valores serão inseridos na pilha: ");
+		scanf("%d", &entrada);
+		fflush(stdin);
+		if (entrada == 0) {
+			printf("Número inválido");
 		} else {
-			printf("Digite um valor para ser empilhado: ");
-			scanf("%d",&pilha.dados[pilha.fim]);
-			fflush(stdin);
-			pilha.fim++;
+			for (int i = 0; i < entrada; i++) {
+				if (pilha.fim == tamanho) {
+
+					printf("A pilha está cheia\n");
+					return 0;
+				} else {
+					printf("Digite um valor para ser empilhado: ");
+					scanf("%d",&pilha.dados[pilha.fim]);
+					fflush(stdin);
+					pilha.fim++;
+				}
+			}
 		}
 	}
 
-
-
-
-	for (int i = 0; i < 10; i++) {
-		pilha_entrada();
+	/* Saída de valores empilhados */
+	void pilha_retirar() {
+		if (pilha.inicio == pilha.fim) {
+			printf("A pilha está vazia\n");
+		} else {
+			pilha.dados[pilha.fim-1] = 0;
+			pilha.fim--;
+		}
 	}
-	pilha_entrada();
-			
+
+	while (sair != 0) {
+
+		printf("\n0 - Sair\n1 - Empilhar\n2 - Desempilhar\n3 - Visualizar Pilha\n");
+		scanf("%d", &menu);
+
+		switch (menu) 
+		{
+		case 3:
+			pilha_conteudo();
+			break;
+		
+		case 2:
+			pilha_retirar();
+			break;
+
+		case 1:
+			pilha_entrada();
+			break;
+
+		case 0:
+			return 0;
+			break;
+		
+		default:
+			break;
+		}
+
+	}
+		
 	return 0;
 }
--- valor-dividendo/dividendo.c
+++ valor-dividendo/dividendo.c
@@ -3,29 +3,29 @@
 #include <locale.h>
 
 void main(){
-    /* D = Dividendo, d = Divisor, q = Quociente, r = Resto */
+    
 	int dividendo, divisor, quociente, resto;
 
 	setlocale(LC_ALL,"Portuguese");
+
 	printf("Digite o valor do Dividendo: ");
 	scanf("%d",&dividendo);
 	printf("Digite o valor do Divisor: ");
 	scanf("%d",&divisor);
+	
 	quociente = dividendo / divisor;
 	resto = dividendo % divisor;
-    if (resto==0 && dividendo>divisor)
-	{
+	
+    if (resto==0) {
 	    printf("O resultado da divisão é: %d\n",quociente);
 	    printf("A divisão é exata\n");
-	}
-	else if (resto>=1 && dividendo>divisor)
-    {
+	} else if (resto>=1 && dividendo>divisor) {
         printf("O resultado da divisão é: %d\n",quociente);
         printf("Divisão não exata com resto igual a: %d\n",resto);
-    }
-    else if (divisor>dividendo) /* As variáveis teriam que ser do tipo float para ser feito uma operação em que o divisor é maior que o dividendo */
-    {
-    	printf("Operação inválida\a\n");
+    } else if (divisor>dividendo) {
+		float resultado = (float) dividendo / (float) divisor;
+		printf("O resultado da divisão é: %.3f\n",resultado);
 	}
 	system("pause");
+
 }
